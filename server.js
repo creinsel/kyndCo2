6d24e4dc8b2cb7fbf2b06caa5d25f467a282be79
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-03-19 20:16:38 -0700

    new repo

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const routes = require("./routes")
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require("mongoose");
+// const router = express.Router();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+// const root = require('path').join(__dirname, 'client', 'build')
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes)
+// app.use(express.static(root));
+
+mongoose.connect("mongodb://localhost/KyndCo", { useNewUrlParser: true }, (err) =>{
+  if(!err) console.log("connected to mongo")
+});
+
+app.get("*", (req, res) => {
+  res.sendFile('index.html', {
+    root
+  });
+})
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
+
+// module.exports = router;
